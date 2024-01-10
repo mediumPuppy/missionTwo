98a865261d3376cb1f59bb209a85818d63f9da54
jl <lambertjefferson66@gmail.com>
2024-01-09 19:34:43 -0800

    Some Minor Updates

--- Program.cs
+++ Program.cs
@@ -13,16 +13,17 @@ internal class Program
         // Line to separate the histogram from the input
         System.Console.WriteLine();
 
-        // Declare array to be counted from
+        // Declare array to be counted from (pulling from Roll() Method
         double[] printArr = Roll(rolls);
 
         // write the lines
         for (int i = 2; i < printArr.Length; i++)
         {
-            double divi = (printArr[i] / rolls) * 100;
-            int myValue = (int)divi;
+            double intMultiplied = (printArr[i] / rolls) * 100;
+            int percentageValue = (int)intMultiplied;
+            // Begin Concatenating Output
             string output = i + ": ";
-            foreach (int x in Enumerable.Range(1, myValue))
+            foreach (int x in Enumerable.Range(1, percentageValue))
             {
                 output += "*"; 
             }
@@ -42,9 +43,10 @@ internal class Program
             // gather random dice in set
             int x = rnd.Next(1, 7);
             int y = rnd.Next(1, 7);
-
+            // Add them together 
             int sum = x + y;
 
+            // add 1 to the position of the value
             myArray[sum] += 1;
         }
 
